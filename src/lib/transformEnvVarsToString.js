78d6150315bb4072cb5453d53a6bb60c67793fc1
Manuel Valenzuela <manuel.valenzuela@moixa.com>
2018-04-26 15:13:24 +0000

    Added env var escaping

--- src/lib/transformEnvVarsToString.js
+++ src/lib/transformEnvVarsToString.js
@@ -3,17 +3,17 @@
 const _ = require("lodash");
 const os = require("os");
 
-
 /**
  * Copies environment variables into a string ready for wrtiting to a file
- * 
+ *
  * @param {String[]} envVars - Environment Variables
  * @returns {String}
  */
 function transformEnvVarsToString(envVars) {
-
 	const output = _.map(envVars, (value, key) => {
-		return `${key}=${value}`;
+		console.log(key);
+		console.log(value);
+		return `${key}="${value.toString().replace(/"/g, '"')}"`;
 	});
 
 	return output.join(os.EOL);
